b0ed1a7498b17dcdbc75487a75cf296f1b350d35
JoKrus <JoKrus@web.de>
2023-07-02 21:47:05 +0200

    Hotfix to OfflinePlayer

--- src/main/java/net/jcom/minecraft/battleplugineventapi/data/TeamConfig.java
+++ src/main/java/net/jcom/minecraft/battleplugineventapi/data/TeamConfig.java
@@ -1,12 +1,12 @@
 package net.jcom.minecraft.battleplugineventapi.data;
 
-import org.bukkit.entity.Player;
+import org.bukkit.OfflinePlayer;
 
 import java.util.HashMap;
 import java.util.List;
 
 public class TeamConfig {
-    public final HashMap<String, List<Player>> teamToPlayers = new HashMap<>();
+    public final HashMap<String, List<OfflinePlayer>> teamToPlayers = new HashMap<>();
     public static final TeamConfig EMPTY = new TeamConfig();
 
     public TeamConfig() {
